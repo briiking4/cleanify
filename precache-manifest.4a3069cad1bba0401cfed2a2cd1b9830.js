3c0de87286a441f88e16c0b1c3309de84ec3b024
briiking4 <45120301+briiking4@users.noreply.github.com>
2020-07-10 19:30:01 -0700

    Updates

---
+++ precache-manifest.4a3069cad1bba0401cfed2a2cd1b9830.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5e2c4b37e0ba0d6de4d1b20be844fc",
+    "url": "/cleanify/index.html"
+  },
+  {
+    "revision": "8a176c921e0c8c85ab67",
+    "url": "/cleanify/static/css/main.a39b946a.chunk.css"
+  },
+  {
+    "revision": "d93653e0dc0440ee43f2",
+    "url": "/cleanify/static/js/2.1326b225.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cleanify/static/js/2.1326b225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a176c921e0c8c85ab67",
+    "url": "/cleanify/static/js/main.7e336358.chunk.js"
+  },
+  {
+    "revision": "5e04030e277b48b9ebbd",
+    "url": "/cleanify/static/js/runtime-main.aa44e632.js"
+  },
+  {
+    "revision": "c00801d94746c66eb3c72d8f1da9ca6f",
+    "url": "/cleanify/static/media/logo.c00801d9.png"
+  },
+  {
+    "revision": "cc02a310228a0c83158dd5c96d803c69",
+    "url": "/cleanify/static/media/profPic.cc02a310.png"
+  }
+]);
