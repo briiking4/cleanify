7bf7c1343570087692cf6c24f8f5053f57515a48
briiking4 <45120301+briiking4@users.noreply.github.com>
2020-07-17 17:16:11 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3aa8a03aa6266a560fa0c94ad92b7d55.js"
+  "/precache-manifest.9ecf2204f479de704dbfdcd6f47e5d46.js"
 );
 
 self.addEventListener('message', (event) => {
